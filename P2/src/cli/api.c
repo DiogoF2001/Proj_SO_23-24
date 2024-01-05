8e550d4c00361299199331a30b69c4e79df17c85
Rodrigo Ingles <rodrigo.ingles@tecnico.ulisboa.pt>
2024-01-05 02:50:35 +0000

    finished api.c

--- P2/src/cli/api.c
+++ P2/src/cli/api.c
@@ -4,67 +4,294 @@
 #include <fcntl.h>
 
 /*Variaveis Globais*/
-int request, response;
+int request, response, session_id;
 
-int ems_setup(char const* req_pipe_path, char const* resp_pipe_path, char const* server_pipe_path) {
-  //TODO: create pipes and connect to the server
-  if (unlink(req_pipe_path) != 0 && errno != ENOENT) {
-        fprintf(stderr, "[ERR]: unlink(%s) failed: %s\n", req_pipe_path,
-                strerror(errno));
-        exit(1);
-    }
+int ems_setup(char const *req_pipe_path, char const *resp_pipe_path, char const *server_pipe_path)
+{
+  // TODO: create pipes and connect to the server
+  if (unlink(req_pipe_path) != 0 && errno != ENOENT)
+  {
+    fprintf(stderr, "[ERR]: unlink(%s) failed: %s\n", req_pipe_path,
+            strerror(errno));
+    exit(1);
+  }
 
-  if (unlink(resp_pipe_path) != 0 && errno != ENOENT) {
-        fprintf(stderr, "[ERR]: unlink(%s) failed: %s\n", resp_pipe_path,
-                strerror(errno));
-        exit(1);
-    }
+  if (unlink(resp_pipe_path) != 0 && errno != ENOENT)
+  {
+    fprintf(stderr, "[ERR]: unlink(%s) failed: %s\n", resp_pipe_path,
+            strerror(errno));
+    exit(1);
+  }
 
-  if (mkfifo(req_pipe_path,0640) != 0 || mkfifo(resp_pipe_path, 0640) !=0) {
-    fprintf(stderr, "[ERR]: mkfifo failed: %s");
+  if (mkfifo(req_pipe_path, 0640) != 0 || mkfifo(resp_pipe_path, 0640) != 0)
+  {
+    fprintf(stderr, "[ERR]: mkfifo failed: %s\n", stderror(errno));
     return 1;
   }
 
-  request = open(req_pipe_path,O_WRONLY);
-	if (request == -1) {
-        fprintf(stderr, "[ERR]: open failed: %s\n", strerror(errno));
-        return 1;
-    }
+  int serv = open(server_pipe_path, O_WRONLY);
+  if (serv == -1)
+  {
+    fprintf(stderr, "[ERR]: open failed: %s\n", strerror(errno));
+    return 1;
+  }
 
-	response= open(resp_pipe_path,O_RDONLY);
-	if (response == -1) {
-        fprintf(stderr, "[ERR]: open failed: %s\n", strerror(errno));
-        return 1;
-    }
+  char *source = malloc(sizeof(char) * (1 + 40 + 40));
+  source[0] = '1';
+  strncy(source + 1, req_pipe_path, strlen(req_pipe_path));
+  for (int i = strlen(req_pipe_path) + 1; i < 40 + 1; i++)
+  {
+    source[i] = '\0';
+  }
+
+  strncy(source + 1 + 40, resp_pipe_path, strlen(resp_pipe_path));
+  for (int i = strlen(resp_pipe_path) + 1 + 40; i < 1 + 40 + 40; i++)
+  {
+    source[i] = '\0';
+  }
+
+  if (write(serv, source, sizeof(source) == -1))
+  {
+    return 1;
+  }
+
+  request = open(req_pipe_path, O_WRONLY);
+  if (request == -1)
+  {
+    fprintf(stderr, "[ERR]: open failed: %s\n", strerror(errno));
+    return 1;
+  }
+
+  response = open(resp_pipe_path, O_RDONLY);
+  if (response == -1)
+  {
+    fprintf(stderr, "[ERR]: open failed: %s\n", strerror(errno));
+    return 1;
+  }
+
+  read(response, &session_id, sizeof(int));
+
+  free(source);
+  close(serv);
 
   return 0;
 }
 
-int ems_quit(void) { 
-  //TODO: close pipes
+int ems_quit(void)
+{
+  // TODO: close pipes
+  if (write(request, "2", sizeof(char) == -1))
+  {
+    return 1;
+  }
+
+  if (write(request, &session_id, sizeof(int) == -1))
+  {
+    return 1;
+  }
+
   close(request);
   close(response);
 
-  return 1;
+  return 0;
 }
 
-int ems_create(unsigned int event_id, size_t num_rows, size_t num_cols) {
-  //TODO: send create request to the server (through the request pipe) and wait for the response (through the response pipe)
+int ems_create(unsigned int event_id, size_t num_rows, size_t num_cols)
+{
+  // TODO: send create request to the server (through the request pipe) and wait for the response (through the response pipe)
+  int ret;
 
-  return 1;
+  if (write(request, "3", sizeof(char) == -1))
+  {
+    return 1;
+  }
+
+  if (write(request, &session_id, sizeof(int) == -1))
+  {
+    return 1;
+  }
+
+  if (write(request, &event_id, sizeof(unsigned int)) != sizeof(unsigned int))
+  {
+    return 1;
+  }
+
+  if (write(request, &num_rows, sizeof(size_t)) != sizeof(size_t))
+  {
+    return 1;
+  }
+
+  if (write(request, &num_cols, sizeof(size_t)) != sizeof(size_t))
+  {
+    return 1;
+  }
+
+  if (read(response, &ret, sizeof(int)) == -1)
+    return 1;
+
+  return 0;
 }
 
-int ems_reserve(unsigned int event_id, size_t num_seats, size_t* xs, size_t* ys) {
-  //TODO: send reserve request to the server (through the request pipe) and wait for the response (through the response pipe)
-  return 1;
+int ems_reserve(unsigned int event_id, size_t num_seats, size_t *xs, size_t *ys)
+{
+  // TODO: send reserve request to the server (through the request pipe) and wait for the response (through the response pipe)
+  int ret;
+  size_t val;
+
+  if (write(request, "4", sizeof(char) == -1))
+  {
+    return 1;
+  }
+
+  if (write(request, &session_id, sizeof(int) == -1))
+  {
+    return 1;
+  }
+
+  if (write(request, &event_id, sizeof(unsigned int)) != sizeof(unsigned int))
+  {
+    return 1;
+  }
+
+  if (write(request, &num_seats, sizeof(size_t)) != sizeof(size_t))
+  {
+    return 1;
+  }
+
+  for (int i = 0; i < num_seats; i++)
+  {
+    val = xs[i];
+    if (write(request, &val, sizeof(size_t)) != sizeof(size_t))
+    {
+      return 1;
+    }
+  }
+
+  for (int i = 0; i < num_seats; i++)
+  {
+    val = ys[i];
+    if (write(request, &val, sizeof(size_t)) != sizeof(size_t))
+    {
+      return 1;
+    }
+  }
+
+  if (read(response, &ret, sizeof(int)) == -1)
+    return 1;
+
+  if (ret == num_seats)
+    return 1;
+
+  return 0;
 }
 
-int ems_show(int out_fd, unsigned int event_id) {
-  //TODO: send show request to the server (through the request pipe) and wait for the response (through the response pipe)
-  return 1;
+int ems_show(int out_fd, unsigned int event_id)
+{
+  // TODO: send show request to the server (through the request pipe) and wait for the response (through the response pipe)
+  int ret;
+  unsigned int seat;
+  size_t rows, cols;
+
+  if (write(request, "5", sizeof(char) == -1))
+  {
+    return 1;
+  }
+
+  if (write(request, &session_id, sizeof(int) == -1))
+  {
+    return 1;
+  }
+
+  if (write(request, &event_id, sizeof(unsigned int)) != sizeof(unsigned int))
+  {
+    return 1;
+  }
+
+  if (read(response, &ret, sizeof(int)) == -1)
+  {
+    return 1;
+  }
+
+  if (ret == 1)
+  {
+    fprintf(stderr, "[ERR]: Event not found: %s\n", strerror(errno));
+    return 1;
+  }
+
+  if (read(response, &rows, sizeof(size_t)) == -1)
+  {
+    return 1;
+  }
+
+  if (read(response, &cols, sizeof(size_t)) == -1)
+  {
+    return 1;
+  }
+
+  for (size_t i = 0; i < rows; i++)
+  {
+    for (size_t j = 0; j < cols; j++)
+    {
+      read(response, &seat, sizeof(seat));
+      write(out_fd, &seat, sizeof(seat));
+
+      if (j < cols)
+      {
+        write(out_fd, " ", sizeof(char));
+      }
+    }
+
+    write(out_fd, "\n", sizeof(char));
+  }
+
+  return 0;
 }
 
-int ems_list_events(int out_fd) {
-  //TODO: send list request to the server (through the request pipe) and wait for the response (through the response pipe)
-  return 1;
+int ems_list_events(int out_fd)
+{
+  // TODO: send list request to the server (through the request pipe) and wait for the response (through the response pipe)
+  int ret;
+  size_t num_events;
+  unsigned int event_id;
+
+  if (write(request, "6", sizeof(char) == -1))
+  {
+    return 1;
+  }
+
+  if (write(request, &session_id, sizeof(int) == -1))
+  {
+    return 1;
+  }
+
+  if (read(response, &ret, sizeof(int)) == -1)
+  {
+    return 1;
+  }
+
+  if (ret == 1)
+  {
+    fprintf(stderr, "[ERR]: Event not found: %s\n", strerror(errno));
+    return 1;
+  }
+
+  if (read(response, &num_events, sizeof(int)) == -1)
+  {
+    return 1;
+  }
+
+  if (num_events == 0)
+  {
+    write(out_fd, "No events.", sizeof("No events.\n"));
+    return 1;
+  }
+
+  for (size_t i = 0; i < num_events; i++)
+  {
+    read(response, &event_id, sizeof(event_id));
+    write(out_fd, "Event: ", sizeof("Event: "));
+    write(out_fd, i, sizeof(int));
+    write(out_fd, "\n", sizeof(char));
+  }
+  return 0;
 }
--- P2/src/serv/operations.c
+++ P2/src/serv/operations.c
@@ -13,7 +13,8 @@ static pthread_rwlock_t *list_lock;
 /// Calculates a timespec from a delay in milliseconds.
 /// @param delay_ms Delay in milliseconds.
 /// @return Timespec with the given delay.
-static struct timespec delay_to_timespec(unsigned int delay_ms) {
+static struct timespec delay_to_timespec(unsigned int delay_ms)
+{
   return (struct timespec){delay_ms / 1000, (delay_ms % 1000) * 1000000};
 }
 
@@ -22,7 +23,8 @@ static struct timespec delay_to_timespec(unsigned int delay_ms) {
 /// resource.
 /// @param event_id The ID of the event to get.
 /// @return Pointer to the event if found, NULL otherwise.
-static struct Event *get_event_with_delay(unsigned int event_id) {
+static struct Event *get_event_with_delay(unsigned int event_id)
+{
   struct Event *ret;
   struct timespec delay = delay_to_timespec(state_access_delay_ms);
   nanosleep(&delay, NULL); // Should not be removed
@@ -40,7 +42,8 @@ static struct Event *get_event_with_delay(unsigned int event_id) {
 /// @param event Event to get the seat from.
 /// @param index Index of the seat to get.
 /// @return Pointer to the seat.
-static unsigned int *get_seat_with_delay(struct Event *event, size_t index) {
+static unsigned int *get_seat_with_delay(struct Event *event, size_t index)
+{
   struct timespec delay = delay_to_timespec(state_access_delay_ms);
   nanosleep(&delay, NULL); // Should not be removed
 
@@ -53,12 +56,15 @@ static unsigned int *get_seat_with_delay(struct Event *event, size_t index) {
 /// @param row Row of the seat.
 /// @param col Column of the seat.
 /// @return Index of the seat.
-static size_t seat_index(struct Event *event, size_t row, size_t col) {
+static size_t seat_index(struct Event *event, size_t row, size_t col)
+{
   return (row - 1) * event->cols + col - 1;
 }
 
-int ems_init(unsigned int delay_ms) {
-  if (event_list != NULL) {
+int ems_init(unsigned int delay_ms)
+{
+  if (event_list != NULL)
+  {
     fprintf(stderr, "EMS state has already been initialized\n");
     return 1;
   }
@@ -67,7 +73,8 @@ int ems_init(unsigned int delay_ms) {
   state_access_delay_ms = delay_ms;
 
   list_lock = malloc(sizeof(pthread_rwlock_t));
-  if (list_lock == NULL) {
+  if (list_lock == NULL)
+  {
     fprintf(stderr, "Error allocating memory for list_lock\n");
     return 1;
   }
@@ -76,8 +83,10 @@ int ems_init(unsigned int delay_ms) {
   return event_list == NULL;
 }
 
-int ems_terminate() {
-  if (event_list == NULL) {
+int ems_terminate()
+{
+  if (event_list == NULL)
+  {
     fprintf(stderr, "EMS state must be initialized\n");
     return 1;
   }
@@ -91,20 +100,24 @@ int ems_terminate() {
   return 0;
 }
 
-int ems_create(unsigned int event_id, size_t num_rows, size_t num_cols) {
-  if (event_list == NULL) {
+int ems_create(unsigned int event_id, size_t num_rows, size_t num_cols)
+{
+  if (event_list == NULL)
+  {
     fprintf(stderr, "EMS state must be initialized\n");
     return 1;
   }
 
-  if (get_event_with_delay(event_id) != NULL) {
+  if (get_event_with_delay(event_id) != NULL)
+  {
     fprintf(stderr, "Event already exists\n");
     return 1;
   }
 
   struct Event *event = malloc(sizeof(struct Event));
 
-  if (event == NULL) {
+  if (event == NULL)
+  {
     fprintf(stderr, "Error allocating memory for event\n");
     return 1;
   }
@@ -115,41 +128,47 @@ int ems_create(unsigned int event_id, size_t num_rows, size_t num_cols) {
   event->reservations = 0;
   event->data = malloc(num_rows * num_cols * sizeof(unsigned int));
 
-  if (event->data == NULL) {
+  if (event->data == NULL)
+  {
     fprintf(stderr, "Error allocating memory for event data\n");
     free(event);
     return 1;
   }
   pthread_rwlock_init(&(event->event_lock), NULL);
 
-  for (size_t i = 0; i < num_rows * num_cols; i++) {
+  for (size_t i = 0; i < num_rows * num_cols; i++)
+  {
     event->data[i] = 0;
   }
 
   pthread_rwlock_wrlock(list_lock);
 
-  if(append_to_list(event_list, event) != 0) {
-	pthread_rwlock_unlock(list_lock);
+  if (append_to_list(event_list, event) != 0)
+  {
+    pthread_rwlock_unlock(list_lock);
     fprintf(stderr, "Error appending event to list\n");
-	pthread_rwlock_destroy(&(event->event_lock));
+    pthread_rwlock_destroy(&(event->event_lock));
     free(event->data);
     free(event);
-	pthread_rwlock_unlock(list_lock);
+    pthread_rwlock_unlock(list_lock);
     return 1;
   }
   pthread_rwlock_unlock(list_lock);
   return 0;
 }
 
-int ems_reserve(unsigned int event_id, size_t num_seats, size_t *xs, size_t *ys) {
-  if (event_list == NULL) {
+int ems_reserve(unsigned int event_id, size_t num_seats, size_t *xs, size_t *ys)
+{
+  if (event_list == NULL)
+  {
     fprintf(stderr, "EMS state must be initialized\n");
     return 1;
   }
 
   struct Event *event = get_event_with_delay(event_id);
 
-  if (event == NULL) {
+  if (event == NULL)
+  {
     fprintf(stderr, "Event not found\n");
     return 1;
   }
@@ -159,16 +178,19 @@ int ems_reserve(unsigned int event_id, size_t num_seats, size_t *xs, size_t *ys)
   unsigned int reservation_id = ++event->reservations;
 
   size_t i = 0;
-  for (; i < num_seats; i++) {
+  for (; i < num_seats; i++)
+  {
     size_t row = xs[i];
     size_t col = ys[i];
 
-    if (row <= 0 || row > event->rows || col <= 0 || col > event->cols) {
+    if (row <= 0 || row > event->rows || col <= 0 || col > event->cols)
+    {
       fprintf(stderr, "Invalid seat\n");
       break;
     }
 
-    if (*get_seat_with_delay(event, seat_index(event, row, col)) != 0) {
+    if (*get_seat_with_delay(event, seat_index(event, row, col)) != 0)
+    {
       fprintf(stderr, "Seat already reserved\n");
       break;
     }
@@ -177,13 +199,15 @@ int ems_reserve(unsigned int event_id, size_t num_seats, size_t *xs, size_t *ys)
   }
 
   // If the reservation was not successful, free the seats that were reserved.
-  if (i < num_seats){
+  if (i < num_seats)
+  {
     event->reservations--;
-    for (size_t j = 0; j < i; j++) {
-      if(*get_seat_with_delay(event, seat_index(event, xs[j], ys[j])) == 0)
-	    *get_seat_with_delay(event, seat_index(event, xs[j], ys[j])) = 0;
+    for (size_t j = 0; j < i; j++)
+    {
+      if (*get_seat_with_delay(event, seat_index(event, xs[j], ys[j])) == 0)
+        *get_seat_with_delay(event, seat_index(event, xs[j], ys[j])) = 0;
     }
-	pthread_rwlock_unlock(&(event->event_lock));
+    pthread_rwlock_unlock(&(event->event_lock));
     return 1;
   }
   pthread_rwlock_unlock(&(event->event_lock));
@@ -191,19 +215,22 @@ int ems_reserve(unsigned int event_id, size_t num_seats, size_t *xs, size_t *ys)
   return 0;
 }
 
-int ems_show(unsigned int event_id, int fd) {
+int ems_show(unsigned int event_id, int fd)
+{
   int ret = 1;
-  if (event_list == NULL) {
+  if (event_list == NULL)
+  {
     fprintf(stderr, "EMS state must be initialized\n");
-	write(fd, &ret, sizeof(int));
+    write(fd, &ret, sizeof(int));
     return 1;
   }
 
   struct Event *event = get_event_with_delay(event_id);
 
-  if (event == NULL) {
+  if (event == NULL)
+  {
     fprintf(stderr, "Event not found\n");
-	write(fd, &ret, sizeof(int));
+    write(fd, &ret, sizeof(int));
     return 1;
   }
 
@@ -211,9 +238,13 @@ int ems_show(unsigned int event_id, int fd) {
 
   ret = 0;
   write(fd, &ret, sizeof(int));
+  write(fd, &(event->rows), sizeof(size_t));
+  write(fd, &(event->cols), sizeof(size_t));
 
-  for (size_t i = 1; i <= event->rows; i++){
-    for (size_t j = 1; j <= event->cols; j++){
+  for (size_t i = 1; i <= event->rows; i++)
+  {
+    for (size_t j = 1; j <= event->cols; j++)
+    {
       unsigned int *seat = get_seat_with_delay(event, seat_index(event, i, j));
       write(fd, seat, sizeof(unsigned int));
     }
@@ -223,38 +254,43 @@ int ems_show(unsigned int event_id, int fd) {
   return 0;
 }
 
-int ems_list_events(int fd) {
+int ems_list_events(int fd)
+{
   int ret = 1;
   size_t num_events = 0;
 
   pthread_rwlock_rdlock(list_lock);
 
-  if (event_list == NULL) {
+  if (event_list == NULL)
+  {
     fprintf(stderr, "EMS state must be initialized\n");
-	pthread_rwlock_unlock(list_lock);
-	write(fd, &ret, sizeof(int));
+    pthread_rwlock_unlock(list_lock);
+    write(fd, &ret, sizeof(int));
     return 1;
   }
 
-  if (event_list->head == NULL) {
-	pthread_rwlock_unlock(list_lock);
-	ret = 0;
-	write(fd, &ret, sizeof(int));
-	// write to pipe no events exist
-	write(fd, &num_events, sizeof(size_t));
+  if (event_list->head == NULL)
+  {
+    pthread_rwlock_unlock(list_lock);
+    ret = 0;
+    write(fd, &ret, sizeof(int));
+    // write to pipe no events exist
+    write(fd, &num_events, sizeof(size_t));
     return 0;
   }
 
   struct ListNode *current = event_list->head;
-  while (current != NULL) {
+  while (current != NULL)
+  {
     current = current->next;
-	num_events++;
+    num_events++;
   }
   write(fd, &num_events, sizeof(size_t));
 
   current = event_list->head;
-  while(current != NULL){
-	write(fd, &(current->event->id), sizeof(unsigned int));
+  while (current != NULL)
+  {
+    write(fd, &(current->event->id), sizeof(unsigned int));
     current = current->next;
   }
 
